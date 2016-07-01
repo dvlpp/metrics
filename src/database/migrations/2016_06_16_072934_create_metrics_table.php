cf41c0199ff917bfb839fd41f976178a94c68cc6
Rémi Collin <rcollin2@gmail.com>
2016-07-01 14:54:20 +0000

    Change Migration class name

--- src/database/migrations/2016_06_16_072934_create_metrics_table.php
+++ src/database/migrations/2016_06_16_072934_create_metrics_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateMetricsTable extends Migration
+class CreateMetricsStatTable extends Migration
 {
     /**
      * Run the migrations.
