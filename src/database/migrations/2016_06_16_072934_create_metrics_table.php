36c5c3ac779ab36fc7b934f363059d1ff2785982
Rémi Collin <remi@plateau.io>
2016-08-29 11:01:34 +0200

    Fix migration class name

--- src/database/migrations/2016_06_16_072934_create_metrics_table.php
+++ src/database/migrations/2016_06_16_072934_create_metrics_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateMetricsStatTable extends Migration
+class CreateMetricsTable extends Migration
 {
     /**
      * Run the migrations.
